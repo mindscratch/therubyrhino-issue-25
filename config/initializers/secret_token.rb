7e87cfc534e65a886b7c054d8ef4d9cbc848ee04
Craig Wickesser <craig@mindscratch.org>
2012-08-22 20:17:13 -0400

    initial app with dependencies on therubyrhino and less-rails-bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyrhino::Application.config.secret_token = 'b211d4633c737f1acc6a10a9995405a2ae4b4b0c6844cbfe836924872fa96d6de7819bf8aa640bc6eb014bca7f99a180566a8b4afb31ab95d4faa50f4b043bc4'
